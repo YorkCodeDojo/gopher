84b1d7fea3e872cb40473dfd16388b30aae8f513
davidbetteridge <david.betteridge@zeromission.io>
2026-03-11 09:59:24 +0000

    wip

---
+++ David_CSharp/David_CSharp_Client/GopherClient.cs
@@ -0,0 +1,65 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace David_CSharp_Client;
+
+public class GopherClient : IDisposable
+{
+    private readonly Socket _socket;
+
+    private GopherClient(Socket socker)
+    {
+        _socket = socker;
+    }
+
+    public static async Task<GopherClient> ConnectAsync(Uri address)
+    {
+        var localhost = await Dns.GetHostEntryAsync(address.DnsSafeHost);
+        var localIpAddress = localhost.AddressList[0];
+
+        IPEndPoint ipEndPoint = new(localIpAddress, 70);
+
+        Socket client = new(
+            ipEndPoint.AddressFamily,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+
+        await client.ConnectAsync(ipEndPoint);
+
+        return new GopherClient(client);
+    }
+
+
+    public async Task<List<string>> GetLinesAsync(string selector)
+    {
+        var messageBytes = Encoding.UTF8.GetBytes(selector + System.Environment.NewLine);
+        _ = await _socket.SendAsync(messageBytes, SocketFlags.None);
+
+        var buffer = new byte[1_024];
+        int received;
+        var all = "";
+        do
+        {
+            received = await _socket.ReceiveAsync(buffer, SocketFlags.None);
+            if (received > 0)
+            {
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+                if (response != ".")
+                {
+                    all += response;
+                }
+            }
+        } while (received > 0 );
+
+        
+        return all.Split(System.Environment.NewLine).ToList();
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        _socket.Close();
+        _socket.Dispose();
+    }
+}
---
+++ David_CSharp/David_CSharp_Client/Models.cs
@@ -0,0 +1,86 @@
+namespace David_CSharp_Client;
+
+public interface ILine
+{
+}
+
+public class Information : ILine
+{
+    public required string Text { get; init; }
+
+    public override string ToString()
+    {
+        //ito spidering.          error.host      1
+        var parts = Text.Split('\t');
+        return parts[0];
+    }
+}
+
+public class TextFile : ILine
+{
+    public string Text { get; }
+    public string Selector { get; }
+    public string Host { get; }
+    
+    private readonly string _fullLine;
+
+    /// <summary>
+    /// 1CIA World Factbook     /Archives/mirrors/textfiles.com/politics/CIA    gopher.quux.org 70
+    /// </summary>
+    public TextFile(string fullLine)
+    {
+        _fullLine = fullLine;
+        var parts = fullLine.Split('\t');
+
+        Text = parts[0];
+        Selector = parts[1];
+        Host = parts[2];
+    }
+
+    public override string ToString()
+    {
+        
+        return _fullLine;
+    }
+}
+
+public class SubMenu : ILine
+{
+    /// <summary>
+    /// 1CIA World Factbook     /Archives/mirrors/textfiles.com/politics/CIA    gopher.quux.org 70
+    /// </summary>
+    public required string Text { get; init; }
+
+    public override string ToString()
+    {
+        var parts = Text.Split('\t');
+        return parts[0];
+    }
+}
+
+public class Unknown : ILine
+{
+    public required string Text { get; init; }
+
+    public override string ToString()
+    {
+        return "Unknown -- " + Text;
+    }
+}
+
+public static class GopherParser
+{
+    public static ILine Parse(string line)
+    {
+        if (line.StartsWith('i'))
+            return new Information { Text = line[1..] };
+
+        if (line.StartsWith('0'))
+            return new TextFile(line[1..]);
+        
+        if (line.StartsWith('1'))
+            return new SubMenu { Text = line[1..] };
+        
+        return new Unknown { Text = line };
+    }
+}
