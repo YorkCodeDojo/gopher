3dc733e8ab28428bdfe4cda0629fc456eb1d9b28
davidbetteridge <david.betteridge@zeromission.io>
2026-03-11 10:42:26 +0000

    wip

---
+++ David_CSharp/David_CSharp.Server/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var hostName = Dns.GetHostName();
+var localhost = await Dns.GetHostEntryAsync(hostName);
+var localIpAddress = localhost.AddressList[0];
+
+IPEndPoint ipEndPoint = new(localIpAddress, 70);
+
+using var listener = new Socket(
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+listener.Bind(ipEndPoint);
+listener.Listen(100);
+
+var handler = await listener.AcceptAsync();
+while (true)
+{
+    // Receive message.
+    var buffer = new byte[1_024];
+    var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+    var request = Encoding.UTF8.GetString(buffer, 0, received);
+    Console.WriteLine(request);
+    if (string.IsNullOrWhiteSpace(request)) continue;
+    if (!request.StartsWith('/')) continue;
+    if (request.Contains("..")) continue;
+    
+    // Paths are relative to /Users/davidbetteridge
+    // ie. /pictures means /Users/davidbetteridge/pictures
+    var fullPath = Path.Combine("/Users/davidbetteridge", request);
+    Console.WriteLine(fullPath);
+    
+    if (!Directory.Exists(fullPath)) continue;
+
+    foreach (var subfolder in Directory.GetDirectories(fullPath))
+    {
+        var folderName = Path.GetFileNameWithoutExtension(subfolder);
+        var ackMessage = $"1{folderName}\t/{folderName}\tlocalhost\t70";
+        var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
+        await handler.SendAsync(echoBytes, 0);        
+    }
+    
+    // var eom = "<|EOM|>";
+    // if (response.IndexOf(eom) > -1 /* is end of message */)
+    // {
+    //     Console.WriteLine(
+    //         $"Socket server received message: \"{response.Replace(eom, "")}\"");
+    //
+    //     var ackMessage = "<|ACK|>";
+    //     var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
+    //     await handler.SendAsync(echoBytes, 0);
+    //     Console.WriteLine(
+    //         $"Socket server sent acknowledgment: \"{ackMessage}\"");
+    //
+    //     break;
+    // }
+    // // Sample output:
+    //    Socket server received message: "Hi friends 👋!"
+    //    Socket server sent acknowledgment: "<|ACK|>"
+}
---
+++ David_CSharp/David_CSharp_Client/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using David_CSharp_Client;
+using Spectre.Console;
+
+var server = new Uri("gopher://gopher.floodgap.com");
+var client = await GopherClient.ConnectAsync(server);
+
+await RenderPageAsync(client, "");
+
+return;
+
+async Task RenderPageAsync(GopherClient gopherClient, string selector)
+{
+    var resourceNumber = 0;
+
+    var links = new List<ILine>(); // off by one!
+
+    foreach (var line in await gopherClient.GetLinesAsync(selector))
+    {
+        var parsedLine = GopherParser.Parse(line);
+
+        var colour = parsedLine switch
+        {
+            Information => Color.Blue,
+            TextFile => Color.Green,
+            SubMenu => Color.Yellow,
+            _ => Color.Red,
+        };
+
+        var text = parsedLine switch
+        {
+            Information => parsedLine.ToString(),
+            TextFile file => $"(TEXTFILE {++resourceNumber}) " + file.Text,
+            SubMenu menu => $"(SUBMENU {++resourceNumber}) " + menu.ToString(),
+            _ => parsedLine.ToString(),
+        };
+    
+        if (parsedLine is TextFile or SubMenu)
+            links.Add(parsedLine);
+    
+        var styled = new Text(text ?? string.Empty, new Style(foreground: colour));
+        AnsiConsole.Write(styled);
+        AnsiConsole.WriteLine();
+    }
+    
+    var requestedResource = AnsiConsole.Ask<string>($"Navigate to: [[1-{resourceNumber}]]");
+    var resource = links[int.Parse(requestedResource) - 1];
+
+    if (resource is TextFile tf)
+        await RenderPageAsync(gopherClient, tf.Selector);
+    else
+        Console.WriteLine(resource);
+}
+
